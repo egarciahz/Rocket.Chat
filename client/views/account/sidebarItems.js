3daa024c50fdd64a7a305248eec10f9c330468fa
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2021-12-05 23:47:08 -0400

    hide user account options on sidebar

--- client/views/account/sidebarItems.js
+++ client/views/account/sidebarItems.js
@@ -1,4 +1,4 @@
-import { hasPermission } from '../../../app/authorization/client';
+// import { hasPermission } from '../../../app/authorization/client';
 import { settings } from '../../../app/settings/client';
 import { createSidebarItems } from '../../lib/createSidebarItems';
 
@@ -20,26 +20,26 @@ export const {
 		icon: 'user',
 		permissionGranted: () => settings.get('Accounts_AllowUserProfileChange'),
 	},
-	{
-		pathSection: 'account',
-		pathGroup: 'security',
-		i18nLabel: 'Security',
-		icon: 'lock',
-		permissionGranted: () =>
-			settings.get('Accounts_TwoFactorAuthentication_Enabled') || settings.get('E2E_Enable'),
-	},
-	{
-		pathSection: 'account',
-		pathGroup: 'integrations',
-		i18nLabel: 'Integrations',
-		icon: 'code',
-		permissionGranted: () => settings.get('Webdav_Integration_Enabled'),
-	},
-	{
-		pathSection: 'account',
-		pathGroup: 'tokens',
-		i18nLabel: 'Personal_Access_Tokens',
-		icon: 'key',
-		permissionGranted: () => hasPermission('create-personal-access-tokens'),
-	},
+	// {
+	// 	pathSection: 'account',
+	// 	pathGroup: 'security',
+	// 	i18nLabel: 'Security',
+	// 	icon: 'lock',
+	// 	permissionGranted: () =>
+	// 		settings.get('Accounts_TwoFactorAuthentication_Enabled') || settings.get('E2E_Enable'),
+	// },
+	// {
+	// 	pathSection: 'account',
+	// 	pathGroup: 'integrations',
+	// 	i18nLabel: 'Integrations',
+	// 	icon: 'code',
+	// 	permissionGranted: () => settings.get('Webdav_Integration_Enabled'),
+	// },
+	// {
+	// 	pathSection: 'account',
+	// 	pathGroup: 'tokens',
+	// 	i18nLabel: 'Personal_Access_Tokens',
+	// 	icon: 'key',
+	// 	permissionGranted: () => hasPermission('create-personal-access-tokens'),
+	// },
 ]);
